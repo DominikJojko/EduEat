1fd10462fbd0071e55ece03eb242bb23384ca6f3
Dominik Jojko <20dominik01@gmail.com>
2024-09-01 10:41:44 +0200

    adminpanel wysrodkkowanie przycisków

--- zamawianie-obiadow/src/components/AdminPanel/AdminPanel.js
+++ zamawianie-obiadow/src/components/AdminPanel/AdminPanel.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import './AdminPanel.css';
 
 function AdminPanel() {
   const { user } = useAuth();
+  const navigate = useNavigate();
 
   if (!user || user.role_id !== 3) {
     return <div>Dostęp ograniczony. Strona dostępna tylko dla administratorów.</div>;
   }
 
   return (
-    <div>
+    <div className="admin-panel-container">
       <h1>Panel Administracyjny</h1>
-      <ul>
-        <li><a href="/admin-panel/list-orders">Generowanie listy zamówionych obiadów</a></li>
-        <li><a href="/admin-panel/add-user">Dodaj i usuń użytkownika</a></li>
-        <li><a href="/admin-panel/manage-classes">Edycja klas</a></li>
-        <li><a href="/admin-panel/make-meals">Zarządzaj obiadami</a></li>
-      </ul>
+      <div className="admin-panel-buttons">
+        <button onClick={() => navigate('/admin-panel/list-orders')}>Generowanie listy zamówionych obiadów</button>
+        <button onClick={() => navigate('/admin-panel/add-user')}>Dodaj i usuń użytkownika</button>
+        <button onClick={() => navigate('/admin-panel/manage-classes')}>Edycja klas</button>
+        <button onClick={() => navigate('/admin-panel/make-meals')}>Zarządzaj obiadami</button>
+      </div>
     </div>
   );
 }
