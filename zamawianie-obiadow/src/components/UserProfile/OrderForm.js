2579177b8aa69219d35fad55e82c37901d56909b
Dominik Jojko <20dominik01@gmail.com>
2024-09-19 01:40:42 +0200

    zmiana przycisku na czerwony gdy zamowiony

--- zamawianie-obiadow/src/components/UserProfile/OrderForm.js
+++ zamawianie-obiadow/src/components/UserProfile/OrderForm.js
@@ -9,8 +9,8 @@ function OrderForm() {
   const [mealDescriptions, setMealDescriptions] = useState([]);
   const [userOrders, setUserOrders] = useState([]);
   const [error, setError] = useState('');
-  const [selectedMonth, setSelectedMonth] = useState('');
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
 
@@ -20,17 +20,17 @@ function OrderForm() {
 
   useEffect(() => {
     const fetchMealDescriptions = async () => {
-      const today = new Date();
-      const now = new Date();
-      const maxDate = new Date();
-      maxDate.setDate(today.getDate() + 14); // obiady na 14 dni do przodu
-
       try {
         const response = await fetch('http://localhost:5000/meal-descriptions');
         if (!response.ok) {
           throw new Error('Nie udało się pobrać opisów obiadów');
         }
         const meals = await response.json();
+        const today = new Date();
+        const now = new Date();
+        const maxDate = new Date();
+        maxDate.setDate(today.getDate() + 14); // obiady na 14 dni do przodu
+
         const filteredMeals = meals.filter(meal => {
           const mealDate = new Date(meal.date);
 
@@ -52,9 +52,9 @@ function OrderForm() {
 
   const fetchUserOrders = async () => {
     if (!user) return;
-    
+
     try {
-      const response = await fetch(`http://localhost:5000/api/user-orders/${user.id}?page=${page}&limit=${limit}`);
+      const response = await fetch(`http://localhost:5000/api/user-orders/${user.id}`);
       if (!response.ok) {
         throw new Error('Nie udało się pobrać zamówień użytkownika');
       }
@@ -67,7 +67,7 @@ function OrderForm() {
 
   useEffect(() => {
     fetchUserOrders();
-  }, [user, page, limit]);
+  }, [user]);
 
   const formatDate = (dateString) => {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -77,25 +77,25 @@ function OrderForm() {
   const handleOrderSubmit = async (meal) => {
     const now = new Date();
     const mealDate = new Date(meal.date);
-  
+
     // Ustawienie godziny 8:30 rano na dzień zamówienia
     const cutoffTime = new Date(mealDate);
     cutoffTime.setHours(8, 30, 0, 0);  // Ustawienie godziny 8:30 rano
-  
+
     // Sprawdzenie, czy obecny czas jest przed 8:30 rano danego dnia
     if (now > cutoffTime) {
       alert(`Możliwość zamówienia obiadu na dzień ${formatDate(meal.date)} już minęła.`);
       return;
     }
-  
+
     const orderDateLocal = mealDate.toISOString().split('T')[0]; // Pobranie daty w formacie YYYY-MM-DD
-  
+
     // Sprawdzanie, czy zamówienie już istnieje
     if (userOrders.some(order => order.date === orderDateLocal)) {
       alert(`Zamówienie na dzień ${formatDate(orderDateLocal)} już istnieje.`);
       return;
     }
-  
+
     console.log("Próba wysyłania zamówienia dla dania o ID:", meal.id, "dla użytkownika o ID:", user.id, "na dzień:", orderDateLocal);
     try {
       const response = await fetch('http://localhost:5000/api/add-order', {
@@ -120,30 +120,55 @@ function OrderForm() {
     }
   };
 
-  const handleMonthOrderSubmit = async () => {
-    if (!selectedMonth) {
-      setError('Wybierz miesiąc, aby złożyć zamówienie.');
+  const handleDateRangeOrderSubmit = async () => {
+    if (!startDate || !endDate) {
+      setError('Wybierz zarówno datę początkową, jak i końcową.');
       return;
     }
-    const confirmation = window.confirm(`Czy na pewno chcesz zamówić obiady na cały ${selectedMonth} ${selectedYear}?`);
-    if (!confirmation) return;
 
-    const month = new Date(`${selectedYear}-${selectedMonth}-01`);
-    const nextMonth = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+    const start = new Date(startDate);
+    const end = new Date(endDate);
 
-    const daysArray = [];
-    for (let day = new Date(month); day <= nextMonth; day.setDate(day.getDate() + 1)) {
-      if (day.getDay() !== 0 && day.getDay() !== 6) { // pomijanie sobót i niedziel
-        daysArray.push(day.toISOString().split('T')[0]);
-      }
+    if (start > end) {
+      setError('Data początkowa nie może być późniejsza niż data końcowa.');
+      return;
     }
 
+    const confirmation = window.confirm(`Czy na pewno chcesz zamówić obiady w przedziale od ${formatDate(startDate)} do ${formatDate(endDate)}?`);
+    if (!confirmation) return;
+
+    // Pobranie opisów obiadów dla wybranego przedziału
     try {
+      const response = await fetch(`http://localhost:5000/meal-descriptions?startDate=${startDate}&endDate=${endDate}`);
+      if (!response.ok) {
+        throw new Error('Nie udało się pobrać opisów obiadów dla wybranego przedziału');
+      }
+      const mealsInRange = await response.json();
+      // Mapowanie posiłków po dacie dla szybkiego dostępu
+      const mealsMap = new Map();
+      mealsInRange.forEach(meal => {
+        mealsMap.set(meal.date, meal);
+      });
+
+      // Aktualizacja zamówień użytkownika
+      await fetchUserOrders();
+
+      const daysArray = [];
+      for (let day = new Date(start); day <= end; day.setDate(day.getDate() + 1)) {
+        if (day.getDay() !== 0 && day.getDay() !== 6) { // pomijanie sobót i niedziel
+          const dateStr = day.toISOString().split('T')[0];
+          daysArray.push(dateStr);
+        }
+      }
+
       for (let date of daysArray) {
-        // Sprawdzanie, czy zamówienie już istnieje
-        const meal = mealDescriptions.find(meal => new Date(meal.date).toISOString().split('T')[0] === date);
-        if (!meal || userOrders.some(order => order.date === date)) {
-          console.log(`Zamówienie na dzień ${formatDate(date)} już istnieje lub brak obiadu.`);
+        const meal = mealsMap.get(date);
+        if (!meal) {
+          console.log(`Brak obiadu na dzień ${formatDate(date)}.`);
+          continue;
+        }
+        if (userOrders.some(order => order.date === date)) {
+          console.log(`Zamówienie na dzień ${formatDate(date)} już istnieje.`);
           continue;
         }
 
@@ -156,47 +181,7 @@ function OrderForm() {
           throw new Error('Błąd podczas wysyłania zamówienia');
         }
       }
-      alert(`Zamówiłeś obiady na cały ${selectedMonth} ${selectedYear}`);
-      fetchUserOrders(); // Pobranie zamówień użytkownika po dodaniu nowych zamówień
-    } catch (error) {
-      alert("Błąd podczas wysyłania zamówienia: " + error.message);
-      console.error("Błąd podczas wysyłania zamówienia:", error);
-    }
-  };
-
-  const handleYearOrderSubmit = async () => {
-    const confirmation = window.confirm('Czy na pewno chcesz zamówić obiady na cały rok szkolny?');
-    if (!confirmation) return;
-
-    const start = new Date(`${selectedYear}-09-01`);
-    const end = new Date(`${selectedYear + 1}-06-30`);
-
-    const daysArray = [];
-    for (let day = new Date(start); day <= end; day.setDate(day.getDate() + 1)) {
-      if (day.getDay() !== 0 && day.getDay() !== 6) { // pomijanie sobót i niedziel
-        daysArray.push(day.toISOString().split('T')[0]);
-      }
-    }
-
-    try {
-      for (let date of daysArray) {
-        // Sprawdzanie, czy zamówienie już istnieje
-        const meal = mealDescriptions.find(meal => new Date(meal.date).toISOString().split('T')[0] === date);
-        if (!meal || userOrders.some(order => order.date === date)) {
-          console.log(`Zamówienie na dzień ${formatDate(date)} już istnieje lub brak obiadu.`);
-          continue;
-        }
-
-        const response = await fetch('http://localhost:5000/api/add-order', {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({ userId: user.id, mealId: meal.id, orderDate: date })
-        });
-        if (!response.ok && response.status !== 409) {
-          throw new Error('Błąd podczas wysyłania zamówienia');
-        }
-      }
-      alert('Zamówiłeś obiady na cały rok szkolny');
+      alert(`Zamówiłeś obiady w przedziale od ${formatDate(startDate)} do ${formatDate(endDate)}`);
       fetchUserOrders(); // Pobranie zamówień użytkownika po dodaniu nowych zamówień
     } catch (error) {
       alert("Błąd podczas wysyłania zamówienia: " + error.message);
@@ -214,43 +199,30 @@ function OrderForm() {
 
   return (
     <div className="order-form-container">
-      <h1><center>Zamówienie Obiadu</center></h1>
-  
-      {/* Zamówienia długoterminowe przeniesione na górę */}
+      <h1>Zamówienie Obiadu</h1>
+
+      {/* Zamówienia długoterminowe */}
       <div className="long-term-order">
         <h2>Zamówienia długoterminowe</h2>
-        <div className="month-order">
+        <div className="date-range-order">
           <label>
-            Wybierz miesiąc:
-            <select value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)}>
-              <option value="">Wybierz miesiąc</option>
-              {Array.from({ length: 10 }, (_, i) => {
-                const month = new Date(0, i + 8).toLocaleString('pl-PL', { month: 'long' });
-                const monthValue = (i + 9).toString().padStart(2, '0');
-                return <option key={monthValue} value={monthValue}>{month}</option>;
-              })}
-            </select>
+            Wybierz datę początkową:
+            <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
           </label>
           <label>
-            Wybierz rok:
-            <select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
-              <option value={new Date().getFullYear()}>{new Date().getFullYear()}</option>
-              <option value={new Date().getFullYear() + 1}>{new Date().getFullYear() + 1}</option>
-            </select>
+            Wybierz datę końcową:
+            <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
           </label>
-          <button onClick={handleMonthOrderSubmit}>Zamów obiady na {selectedMonth ? new Date(0, parseInt(selectedMonth) - 1).toLocaleString('pl-PL', { month: 'long' }) : 'wybrany miesiąc'}</button>
-        </div>
-        <div className="year-order">
-          <button onClick={handleYearOrderSubmit}>Zamów obiady na cały rok szkolny</button>
+          <button onClick={handleDateRangeOrderSubmit}>Zamów obiady w wybranym przedziale</button>
         </div>
       </div>
-  
+
       <div style={{ textAlign: 'center', marginTop: '15px', marginBottom: '20px' }}>
         <a href="" target="_blank" rel="noopener noreferrer">
           <button style={{ padding: '10px 20px', fontSize: '18px', cursor: 'pointer' }}>Menu - zmienic link OrderForm.js</button>
         </a>
       </div>
-  
+
       <table className="meals-table">
         <thead>
           <tr>
@@ -259,21 +231,29 @@ function OrderForm() {
           </tr>
         </thead>
         <tbody>
-          {mealDescriptions.map((meal) => (
-            <tr key={meal.id}>
-              <td>{formatDate(meal.date)}</td>
-              <td>
-                <button onClick={() => handleOrderSubmit(meal)}>Zamów ten obiad</button>
-              </td>
-            </tr>
-          ))}
+          {mealDescriptions.map((meal) => {
+            const mealDateStr = meal.date;
+            const isOrdered = userOrders.some(order => order.date === mealDateStr);
+
+            return (
+              <tr key={meal.id}>
+                <td>{formatDate(meal.date)}</td>
+                <td>
+                  {isOrdered ? (
+                    <button className="already-ordered-button" disabled>Już zamówiony</button>
+                  ) : (
+                    <button onClick={() => handleOrderSubmit(meal)}>Zamów ten obiad</button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-  
+
       {error && <p className="error-message">{error}</p>}
     </div>
   );
-  
 }
 
 export default OrderForm;
